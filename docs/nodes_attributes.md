cf900de9d0748ac7f7b19fb24520dbf4ccb8fd9d
Lilian Penzo <lilian.penzo@gmail.com>
2026-02-12 19:25:27 +0100

    color card and hue saturation added.
    also change a white theme and put in English several comments

--- docs/nodes_attributes.md
+++ docs/nodes_attributes.md
@@ -1,5 +1,3 @@
 # Nodes_Attributes
 
-Esta pagina funciona igual que la referencia de API, pero enfocada en nodos y sus atributos.
-
 ::: nodes_attributes
--- src/nodes_attributes.py
+++ src/nodes_attributes.py
@@ -1,18 +1,44 @@
-"""Referencia estatica de nodos y atributos con tablas."""
+"""Attributes of Nodes from Toon Boom Harmony"""
 
-__all__ = ["Colour_Card", "data"]
-
-
+__all__ = ["Hue_Saturation_Attributtes","Colour_Card_Attributtes"]
 
 
 
-def Colour_Card() -> None:
-    """## Color_Card
+def Hue_Saturation_Attributtes() -> None:
+    """## Hue Saturation Node
 
     **Tipo de Nodo:** COLOR_CARD
 
-    | Keyword | Type | Sub-attributes |
+    Keyword| Keyword | Type | Sub-attributes |
     |---------|------|-----------------|
+    | MasterRangeColor | Attribute |  |
+    | GreenRangeColor | Attribute |  |
+    | RedRangeColor | Attribute |  |
+    | BlueRangeColor | Attribute |  |
+    | CyanRangeColor | Attribute |  |
+    | MagentaRangeColor | Attribute |  |
+    | YellowColorizeEnableRangeColor | BoolAttr |  |
     | ColorizeHsl | Attribute | ColorizeHsl.HUE (DoubleAttr)<br>ColorizeHsl.SATURATION (DoubleAttr)<br>ColorizeHsl.LIGHTNESS (DoubleAttr) |
+    | ResetRed | Attribute |  |
+    | ResetGreen | Attribute |  |
+    | ResetBlue | Attribute |  |
+    | ResetCyan | Attribute |  |
+    | ResetYellow | Attribute |  |
+    | INVERT_MATTE_PORT | BoolAttr |  |
+    """
+    pass
+
+def Colour_Card_Attributtes() -> None:
+    """## Colour Card Node
+
+    **Tipo de Nodo:** COLOR_CARD
+
+    Keyword| Keyword | Type | Sub-attributes |
+    |---------|------|-----------------|
+    | DEPTH | IntAttr |  |
+    | OFFSET_Z | DoubleAttr |  |
+    | COLOR | ColorAttr | COLOR.RED (IntAttr)<br>COLOR.GREEN (IntAttr)<br>COLOR.BLUE (IntAttr)<br>COLOR.ALPHA (IntAttr)<br>COLOR.PREFERRED_UI (EnumAttr)<br> |
+    | INVERT_MATTE_PORT | Attribute |  |
+
     """
     pass
